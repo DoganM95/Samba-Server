eb8d988ada2092ba5a0ee2903fc11d960f7c36f0
DoganM95 <doganmermertas@hotmail.de>
2026-03-06 17:01:58 +0100

    fix shell var expansion by setting the config content from shell script

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,18 +10,33 @@ if ! id "$SAMBA_USER" >/dev/null 2>&1; then
     useradd -u 0 -o -g 0 "$SAMBA_USER"
 fi
 
-# Set Samba password from env var
-(echo "${SAMBA_PASS}"; echo "${SAMBA_PASS}") | smbpasswd -s -a "${SAMBA_USER}"
+# Set Samba password
+(echo "${SAMBA_PASS}"; echo "${SAMBA_PASS}") | smbpasswd -s -a "$SAMBA_USER"
 
 # Ensure storage directory exists
 mkdir -p /storage
 
-# Option A
-# Start smbd in foreground (PID 1)
-# exec /usr/sbin/smbd -F --no-process-group -s /etc/samba/smb.conf
+# Generate smb.conf dynamically
+cat > /etc/samba/smb.conf <<EOL
+[global]
+   workgroup = WORKGROUP
+   server string = Docker Samba Server
+   log file = /var/log/samba/log.%m
+   max log size = 1000
+   security = user
+   map to guest = bad user
 
-# Option B
+[storage]
+   comment = Docker Storage Share
+   path = /storage
+   browseable = yes
+   read only = no
+   guest ok = no
+   valid users = $SAMBA_USER
+EOL
+
 # Start Samba in background
 /usr/sbin/smbd -D --no-process-group -s /etc/samba/smb.conf
+
 # Dummy foreground process to keep container alive
 tail -f /dev/null
